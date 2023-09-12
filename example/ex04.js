edc0a3fda85a4f807bf5b1f9f30ef30912e7a474
Griotold <gotjd9773@naver.com>
2023-09-12 13:24:58 +0900

    반복문 기초 for / for of / map

--- example/ex04.js
+++ example/ex04.js
@@ -11,3 +11,17 @@ let car = {
 
 let array = ["sonata", 1234, car];
 console.log(array[1]);
+
+const points = [40, 100, 1, 5, 25, 10];
+
+for (i = 0; i < points.length; i++) {
+  console.log(points[i]);
+}
+console.log("--------");
+for (point of points) {
+  console.log(point);
+}
+console.log("--------");
+points.map((point) => {
+  console.log(point);
+});
