f58f17c800b06e80d7cbfc3281635403ccdf2776
Griotold <gotjd9773@naver.com>
2023-09-13 16:08:47 +0900

    AccountList 조회 완료

--- fintech/src/pages/AccountList.js
+++ fintech/src/pages/AccountList.js
@@ -1,16 +1,54 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import MainAccountCard from "../components/list/MainAccountCard";
+import axios from "axios";
 
 const AccountList = () => {
+  let accessToken = "";
+  let userSeqNo = "";
+  let [accountList, setAccountList] = useState([]);
+
   useEffect(() => {
-    console.log("data");
+    console.log(localStorage.getItem("accessToken"));
+    console.log(localStorage.getItem("userSeqNo"));
+    accessToken = localStorage.getItem("accessToken");
+    userSeqNo = localStorage.getItem("userSeqNo");
+    getAccountList();
   }, []);
 
-  const getAccountList = () => {};
+  const getAccountList = () => {
+    //axios 요청
+    const sendData = {
+      user_seq_no: userSeqNo,
+    };
+
+    const option = {
+      method: "GET",
+      url: "/v2.0/user/me",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+        Authorization: `Bearer ${accessToken}`,
+      },
+      params: sendData,
+    };
+
+    axios(option).then(({ data }) => {
+      console.log(data.res_list);
+      setAccountList(data.res_list);
+    });
+  };
 
   return (
     <div>
-      <MainAccountCard></MainAccountCard>
+      {accountList.map((account) => {
+        return (
+          <>
+            <MainAccountCard
+              bankName={account.bank_name}
+              fintechUseNo={account.fintech_use_num}
+            ></MainAccountCard>
+          </>
+        );
+      })}
     </div>
   );
 };
