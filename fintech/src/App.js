430ca26b56fbe8b29d42a42b91bd92a5feb34560
Griotold <gotjd9773@naver.com>
2023-09-14 14:52:49 +0900

    QrReader 근데 카메라가 안뜸

--- fintech/src/App.js
+++ fintech/src/App.js
@@ -4,23 +4,18 @@ import AuthResult from "./pages/AuthResult";
 import AccountList from "./pages/AccountList";
 import BalancePage from "./pages/BalancePage";
 import QrCodePage from "./pages/QrCodePage";
+import QrReaderPage from "./pages/QrReaderPage";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        {/* <Route path="/" element={<ListComponent></ListComponent>}></Route>
-        <Route
-          path="/header"
-          element={<HeaderComponent></HeaderComponent>}
-        ></Route>
-        <Route path="/axios" element={<AxiosComponent />}></Route>
-        <Route path="/news" element={<NewsSearch />}></Route> */}
         <Route path="/" element={<MainPage />}></Route>
         <Route path="/authResult" element={<AuthResult />}></Route>
         <Route path="/balance" element={<BalancePage />}></Route>
         <Route path="/list" element={<AccountList />}></Route>
         <Route path="/qrcode" element={<QrCodePage />}></Route>
+        <Route path="/qrreader" element={<QrReaderPage />}></Route>
       </Routes>
     </BrowserRouter>
   );
--- fintech/src/pages/QrCodePage.js
+++ fintech/src/pages/QrCodePage.js
@@ -1,7 +1,34 @@
 import React from "react";
+import HeaderComponent from "../components/HeaderComponent";
+import { QRCodeSVG } from "qrcode.react";
+import { useLocation } from "react-router-dom";
+import styled from "styled-components";
+import queryString from "query-string";
+
+const QRBlock = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 1rem;
+`;
 
 const QrCodePage = () => {
-  return <div>QrCodePage</div>;
+  const queryParams = useLocation().search;
+  const parsed = queryString.parse(queryParams);
+  const fintechNo = parsed.fintechUseNo;
+
+  console.log(queryParams);
+  console.log(parsed);
+  console.log(fintechNo);
+  return (
+    <div>
+      <HeaderComponent title={"QR code"}></HeaderComponent>
+      <QRBlock>
+        <QRCodeSVG size={256} value={fintechNo} />
+        <p>{fintechNo}</p>
+      </QRBlock>
+    </div>
+  );
 };
 
 export default QrCodePage;
