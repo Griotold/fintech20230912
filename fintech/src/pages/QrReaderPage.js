08b7d77224f40f45c2db7843dfdfd2a3925f6dd1
Griotold <gotjd9773@naver.com>
2023-09-14 15:37:26 +0900

    출금이체 입금이체

--- fintech/src/pages/QrReaderPage.js
+++ fintech/src/pages/QrReaderPage.js
@@ -1,16 +1,29 @@
 import React, { useState } from "react";
 import HeaderComponent from "../components/HeaderComponent";
 import QrCodeReader, { QRCode } from "react-qrcode-reader";
+import Modal from "react-modal";
+import ModalWithdraw from "../components/withdraw/ModalWithdraw";
 
 const QrReaderPage = () => {
   const [val, setVal] = useState("");
+  const [openModal, setOpenModal] = useState(false); //no camera : true
+
+  const closeModal = () => {
+    setOpenModal(false);
+  };
+
+  const handleRead = (val) => {
+    setOpenModal(true);
+    setVal(val);
+  };
   return (
     <div>
       <HeaderComponent title={"Reader"}></HeaderComponent>
-      <>
-        <QrCodeReader delay={100} width={600} height={500} action={setVal} />
-        <p>{val}</p>
-      </>
+      <QrCodeReader delay={100} width={600} height={500} action={handleRead} />
+      <Modal isOpen={openModal} onRequestClose={closeModal}>
+        <ModalWithdraw tofintechno={val}></ModalWithdraw>
+      </Modal>
+      {val}
     </div>
   );
 };
